4c8be23a62d2d74afccabf3b087f453b5096fd37
Franz-Filip Schörghuber <S1910307091@students.fh-hagenberg.at>
2022-01-10 17:06:46 +0100

    perfection ala datepipe

--- src/app/ticks-to-date-pipe.ts
+++ src/app/ticks-to-date-pipe.ts
@@ -6,7 +6,9 @@ import { formatDate } from "@angular/common";
 })
 
 export class TicksToDatePipe implements PipeTransform{
-    transform(value: any, ...args: any[]) {
+    transform(value: number): string {
+        //luckily stackoverflow exists
+        //https://stackoverflow.com/questions/15486299/convert-c-sharp-net-datetime-ticks-to-days-hours-mins-in-javascript
         var time = value; // Time value in ticks
         var days = Math.round(time/(24*60*60*10000000)); // Math.floor() rounds a number downwards to the nearest whole integer, which in this case is the value representing the day
         var hours = Math.floor((time/(60*60*10000000)) % 24); // Math.round() rounds the number up or down
@@ -19,6 +21,6 @@ export class TicksToDatePipe implements PipeTransform{
         date.setMinutes(mins);
         date.setSeconds(sec);
 
-        return formatDate(date, "dd.MM.YYYY HH-mm-ss", "en-US");
+        return formatDate(date, "dd.MM.YYYY HH:mm:ss", "en-US");
     }
 }
